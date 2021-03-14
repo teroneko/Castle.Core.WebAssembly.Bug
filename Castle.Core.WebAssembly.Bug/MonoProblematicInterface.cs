d5ad27b60efff952a62277ddccb6e450bd1b54ef
teroneko <giteroneko@gmail.com>
2021-03-14 04:30:50 +0100

    Add project and README.md

---
+++ Castle.Core.WebAssembly.Bug/MonoProblematicInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace Castle.Core.WebAssembly.Bug
+{
+    public interface MonoProblematicInterface
+    {
+        // THIS IS IRONY.
+        public void DiscriminatedFromMonoBecauseImGeneric<T>();
+    }
+}
